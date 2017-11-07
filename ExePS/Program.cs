b4f9c76dd3a546fe3c49b5943f770fe7b73fd601
George Lubomirov <GLubomirov@abilitics.com>
2017-11-07 11:51:42 +0200

    PSToExe
    A VS Project allowing you to run PowerShell Scripts like Executables. Encryption of the PowerShell script is included.

---
+++ ExePS/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ExePS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////YOUR INPUT HERE
+            //The code you used for Protecting the PS Script. Read Guide for details.
+            string protectionCode = "gT4XPfvcJmHkQ5tYjY3fNgi7uwG4FB9j";
+            //The Filename of the Protected Script. This File should be put in the Files folder of the Project with "Embeded Resource" build action.
+            string protectedPSFileName = "WPF.txt";
+
+
+            //string AppFolderUser = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string AppFolderUser = Directory.GetCurrentDirectory();
+            string progFolder = AppFolderUser + @"\PS";
+            if (!(Directory.Exists(progFolder)))
+            {
+                System.IO.Directory.CreateDirectory(progFolder);
+                Extract("ExePS", progFolder, "Files", @"Start.bat");
+                Extract("ExePS", progFolder, "Files", @"Launcher.ps1");
+
+                //Change WPF.txt to the File you produced with Encrypt_Decrypt.ps1. 
+                //The file added to the Files Folder and the Build action should be "Embedded Resource"
+                Extract("ExePS", progFolder, "Files", protectedPSFileName);
+            }
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = progFolder + @"\Start.bat";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.CreateNoWindow = true;
+            startInfo.Arguments = "\"" + protectionCode + "\"" + " " + "\"" + protectedPSFileName + "\"";
+
+            using (Process exeProcess = Process.Start(startInfo))
+            {
+                exeProcess.WaitForExit();
+            }
+        }
+
+        public static void Extract(string nameSpace, string outDirectory, string internalFilePath, string resourceName)
+        {
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            using (Stream s = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName))
+            using (BinaryReader r = new BinaryReader(s))
+            using (FileStream fs = new FileStream(outDirectory + "\\" + resourceName, FileMode.OpenOrCreate))
+            using (BinaryWriter w = new BinaryWriter(fs))
+            {
+                w.Write(r.ReadBytes((int)s.Length));
+            }
+        }
+    }
+}
---
+++ ExePS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExePS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ExePS")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e16532c4-096f-420b-849c-9b053265c7cf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
